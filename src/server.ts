bed6d67f88f3b33f45dc36b17b24fb86c70fc973
Shalan <Mohammedshalan9@gmail.com>
2023-02-04 23:42:52 -0600

    TODO1 done

---
+++ src/server.ts
@@ -0,0 +1,67 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import fs from "fs";
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+app.get('/filteredimage',(req,res)=>{
+  const imageUrl = req.query.image_url 
+  if(!imageUrl)
+    return res.status(500).send('Bad Request. Image url required!')  
+  
+    filterImageFromURL(imageUrl)
+      .then(imagePath=>{
+        fs.readFile(imagePath, (error, data) => {
+          if (error) {
+            return res.status(500).send({ error: 'Failed to read image' });
+          }
+      
+          res.contentType('image/jpeg');
+          res.send(data);
+          deleteLocalFiles([imagePath]);
+        });
+      })
+    
+      
+
+})
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
